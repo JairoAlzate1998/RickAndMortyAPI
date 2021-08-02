8520c9b667f3386d85bd2a1354d710108ed7f950
Jairo Alzate <jairo.alzate@siesa.com>
2021-08-02 09:28:00 -0500

    Add firts cards to page of characters

--- assets/js/ramapi.js
+++ assets/js/ramapi.js
@@ -1,20 +1,38 @@
-// URL API 
+// URL API
 const API = "https://rickandmortyapi.com/api/character";
 
 // Obtener los resultados de la API
-const getData = ()  => {
-
+const getData = (url) => {
+  return fetch(url)
+    .then((response) => response.json())
+    .then((json) => {
+      llenarDatos(json.results), paginacion(json.info);
+    })
+    .catch((error) => {
+      console.log("Error: " + error);
+    });
 };
 
 // Dibujar cards de personajes
-const llenarDatos = () => {
-
+const llenarDatos = (data) => {
+  let html = "";
+  data.forEach((pj) => {
+    html += '<div class="col mt-5">';
+    html += '<div class="card" style="width: 18rem;">';
+    html += `<img src="${pj.image}" class="card-img-top" alt="${pj.name}">`;
+    html += '<div class="card-body">';
+    html += `<h5 class="card-title">${pj.name}</h5>`;
+    html += `<p class="card-text">Status: ${pj.status}</p>`;
+    html += `<p class="card-text">Specie: ${pj.species}</p>`;
+    html += "</div>";
+    html += "</div>";
+    html += "</div>";
+  });
+  document.getElementById("datosPersonajes").innerHTML = html;
 };
 
 // Paginacion
-const paginacion = () => {
-
-};
+const paginacion = () => {};
 
 // Se ejecuta la API
-getData();
+getData(API);
