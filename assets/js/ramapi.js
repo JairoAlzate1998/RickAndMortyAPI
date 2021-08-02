2cf042e4b5d4bf8a458a415232e84bb4a7165b96
Jairo Alzate <jairo.alzate@siesa.com>
2021-08-02 08:17:58 -0500

    Add estructuc of javascript

--- assets/js/ramapi.js
+++ assets/js/ramapi.js
@@ -1,2 +1,20 @@
 // URL API 
 const API = "https://rickandmortyapi.com/api/character";
+
+// Obtener los resultados de la API
+const getData = ()  => {
+
+};
+
+// Dibujar cards de personajes
+const llenarDatos = () => {
+
+};
+
+// Paginacion
+const paginacion = () => {
+
+};
+
+// Se ejecuta la API
+getData();
