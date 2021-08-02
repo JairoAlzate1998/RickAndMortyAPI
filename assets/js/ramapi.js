7a400f0274b96e6992942eaf42b8b8402cc2b687
Jairo Alzate <jairo.alzate@siesa.com>
2021-08-02 10:07:05 -0500

    Add pagination to page

--- assets/js/ramapi.js
+++ assets/js/ramapi.js
@@ -18,7 +18,7 @@ const llenarDatos = (data) => {
   let html = "";
   data.forEach((pj) => {
     html += '<div class="col mt-5">';
-    html += '<div class="card" style="width: 18rem;">';
+    html += '<div class="card" style="width: 10rem;">';
     html += `<img src="${pj.image}" class="card-img-top" alt="${pj.name}">`;
     html += '<div class="card-body">';
     html += `<h5 class="card-title">${pj.name}</h5>`;
@@ -32,7 +32,19 @@ const llenarDatos = (data) => {
 };
 
 // Paginacion
-const paginacion = () => {};
+const paginacion = (data) => {
+    let prevDisabled = "";
+    let nextDisabled = "";
+
+    data.prev == null ? prevDisabled = "disabled" : prevDisabled = "" ;
+    data.next == null ? nextDisabled = "disabled" : nextDisabled = "" ;
+
+    let html = "";
+    html += `<li class="page-item ${prevDisabled}"><a class="page-link" onclick="getData('${data.prev}')">Previous</a></li>`;
+    html += `<li class="page-item ${nextDisabled}"><a class="page-link" onclick="getData('${data.next}')">Next</a></li>`;
+
+    document.getElementById("paginacion").innerHTML = html;
+};
 
 // Se ejecuta la API
 getData(API);
